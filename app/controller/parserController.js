63e1d5887590d243faae6444c4d0cf72a1860c43
Anonymint <mint.com@gmail.com>
2016-01-15 18:13:42 +1100

    Check for edge case

--- app/controller/parserController.js
+++ app/controller/parserController.js
@@ -3,22 +3,29 @@
 var WhoAmI = function() {
 	this.getMe = function(ipaddress, lang, os) {
 		return {
-			"ipaddress" : ipaddress,
-			"language" : lang,
-			"software" : os
+			"ipaddress" : ipaddress || 'null',
+			"language" : lang || 'null',
+			"software" : os || 'null'
 		}
 	}
 }
 
 function Controller() {
 	this.process = function(req) {
+		var ip,lang,os;
+
 		//get ip
-		var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;	
+		ip = req.header('x-forwarded-for') || req.connection.remoteAddress;	
+		
 		//get lang
-		var lang = /(.*),.*/.exec(req.header('accept-language'))[1];
+		if (/(.*),.*/.test(req.header('accept-language'))) {
+			lang = /(.*),.*/.exec(req.header('accept-language'))[1];
+		}		
 
 		//get os
-		var os = (/.*?\((.*?)\)/g).exec(req.header('user-agent'))[1];
+		if ((/.*?\((.*?)\)/g).test(req.header('user-agent'))) {
+			var os = (/.*?\((.*?)\)/g).exec(req.header('user-agent'))[1] ;
+		}		
 
 		var whoAmI = new WhoAmI();
 		return whoAmI.getMe(ip,lang,os);
